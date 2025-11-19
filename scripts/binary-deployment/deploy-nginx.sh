70a268ee12be7d7a39f307d02d17df0fb9c44f4d
0xaditya-ops <aditya@gattaca.com>
2025-11-20 00:48:07 +0530

    Fix nginx installation command in deploy script

--- scripts/binary-deployment/deploy-nginx.sh
+++ scripts/binary-deployment/deploy-nginx.sh
@@ -7,7 +7,7 @@ if [ -f ~/bins/nginx-deployment/env_vars ]; then
 fi
 
 sudo apt-get update
-sudo apt-get install -y "nginx=$NGINX_VERSION-*" || sudo apt-get install -y nginx
+sudo apt-get install -y "nginx=$NGINX_VERSION" || sudo apt-get install -y nginx
 
 if systemctl is-active --quiet nginx; then
     sudo systemctl stop nginx
